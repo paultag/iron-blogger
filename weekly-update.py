a9d51198eb1aaa16a90535afac15592c39992d92
Benjamin Mako Hill <mako@atdot.cc>
2011-11-14 16:30:51 -0500

    reset the ledger after the dry run to the latest git version

--- weekly-update.py
+++ weekly-update.py
@@ -83,3 +83,7 @@ if punt:
 
     subprocess.check_call(["git", "commit", "ledger", "bloggers.yml",
                            "-m", "Punts for %s" % (today,)])
+
+# if it's a dry run, lets set the ledger back to the beginning state
+if dry_run:
+    subprocess.check_call(["git", "checkout", "ledger"])
