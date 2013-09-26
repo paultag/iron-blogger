88e8fcf74237ce172a5d3c6ee50c894866cb9c59
Paul Tagliamonte <tag@pault.ag>
2013-09-25 21:39:20 -0400

    I'm going to try and avoid WP

--- update-participants.py
+++ update-participants.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python
 
 import render
-import os
-import sys
-import xmlrpclib
-import subprocess
-
-from config import *
-
-try:
-    subprocess.call(['stty', '-echo'])
-    passwd = raw_input("Password for %s: " % (USER,))
-    print
-finally:
-    subprocess.call(['stty', 'echo'])
-
-x = xmlrpclib.ServerProxy(XMLRPC_ENDPOINT)
-page = x.wp.getPage(BLOG_ID, PARTICIPANTS_PAGE_ID, USER, passwd)
 
 text = render.render_template('templates/users.tmpl')
-page['description'] = text
-
-x.wp.editPage(BLOG_ID, PARTICIPANTS_PAGE_ID, USER, passwd, page, True)
+open('out/participants.html', 'w').write(text)
