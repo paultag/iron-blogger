8104e3d3773e4e28217d1c3cff52f61fe748094d
Paul Tagliamonte <tag@pault.ag>
2014-01-06 18:32:55 -0500

    move to ferrum

--- meta/devel.sh
+++ meta/devel.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 last=""
 while [ true ]; do
-    now=$(find notes themes/epipe/less themes/epipe/templates themes/epipe/resources -type f -printf "%T@ %Tx %TX %p\n" | sort -n -r | head -1)
+    now=$(find notes themes/ferrum/less themes/ferrum/templates themes/ferrum/resources -type f -printf "%T@ %Tx %TX %p\n" | sort -n -r | head -1)
     if [ "$last" != "$now" ]; then
         make >/dev/null
         echo "Updated."
